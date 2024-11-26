bbc6141d5427aad5ed82473a758223bd117d9edf
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-25 20:28:54 -0500

    Fix the main comment

--- C/main.c
+++ C/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  // Here we will write new things learned from Zig with basic examples
+  // Here we will write new things learned from C with basic examples
   printf("Hello world\n");
   return 0;
 }
