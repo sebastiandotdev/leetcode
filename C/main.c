6bad6b85e0b67ea48d3781bb9e74c83720906ef3
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-28 21:34:32 -0500

    Learn pointers with C

--- C/main.c
+++ C/main.c
@@ -2,6 +2,15 @@
 
 int main() {
   // Here we will write new things learned from C with basic examples
-  printf("Hello world\n");
+  int num = 12;
+
+  int *puntero = &num;
+
+  *puntero = 10;
+
+  printf("Posición de mémoria: %p\n", &puntero);
+  printf("El número original: %d\n", num);
+  printf("El número modificado por puntero: %d\n", *puntero);
+
   return 0;
 }
---
+++ C/run_c.sh
@@ -0,0 +1,6 @@
+rm -rf ./C/main && clang ./C/main.c -o ./C/main
+
+echo "Compiled C code ✅";
+
+./C/main
+
