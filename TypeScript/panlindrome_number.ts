d36a41d76c47b4e93dba5270ca76ccbd6727ae9e
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-03 17:35:53 -0500

    Improve exercise description

---
+++ TypeScript/panlindrome_number.ts
@@ -0,0 +1,29 @@
+// =============================================================================
+// Palindrome Number (Números palindromos)
+// =============================================================================
+//
+// **Enunciado en Inglés:**
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+//
+// **Enunciado en Español:**
+// Dado un número entero x, devuelve verdadero si x es un palíndromo y falso en caso contrario.
+//
+// **Ejemplo:**
+// ```typescript
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// ```
+// =============================================================================
+
+// TODO: Sin hacer mutacion con String
+export function isPalindromeNumber(x: number): boolean {
+  const number = String(x).split("").reverse().join("");
+  const parseNumber = Number.parseInt(number);
+
+  if (Number.isNaN(parseNumber)) return false;
+  if (parseNumber < 0) return false;
+  if (parseNumber !== x) return false;
+
+  return true;
+}
