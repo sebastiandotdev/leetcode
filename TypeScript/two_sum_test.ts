5e97f7fec6d7847fc99ad5b21b73fa1279f97d8a
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-03 17:23:41 -0500

    [TypeScript] Refactor test for `Two Sum`

--- TypeScript/two_sum_test.ts
+++ TypeScript/two_sum_test.ts
@@ -1,17 +1,24 @@
 import { twoSum } from "./two_sum.ts";
-import { assert, equal } from "@std/assert";
+import { assertArrayIncludes } from "@std/assert";
 
 Deno.test("Should return [0, 2]", () => {
-    const arrayIndex = twoSum([2, 11, 7, 15], 9);
-    equal(arrayIndex, [0, 2]);
+  const arrayIndex = twoSum([2, 11, 7, 15], 9) as unknown as Array<number>;
+
+  assertArrayIncludes(arrayIndex, [0, 2]);
+});
+
+Deno.test("Should return [0, 2]", () => {
+  const arrayIndex = twoSum([2, 11, 7, 15], 9) as unknown as Array<number>;
+
+  assertArrayIncludes(arrayIndex, [0, 2]);
 });
 
 Deno.test("SHould return [0, 0]", () => {
-    const arrayIndex = twoSum([4, 4], 8);
-    equal(arrayIndex, [0, 0]);
+  const arrayIndex = twoSum([4, 4], 8) as unknown as Array<number>;
+  assertArrayIncludes(arrayIndex, [0, 0]);
 });
 
 Deno.test("SHould return [1, 2]", () => {
-    const arrayIndex = twoSum([3, 2, 4], 6);
-    console.log(arrayIndex);
+  const arrayIndex = twoSum([3, 2, 4], 6) as unknown as Array<number>;
+  assertArrayIncludes(arrayIndex, [1, 2]);
 });
