bf9246b686740e10dd96bc6754faf0582bf39361
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-03 17:36:33 -0500

    [Test] Evail with diferent cases. `Palindrome number`

---
+++ TypeScript/panlindrome_number_test.ts
@@ -0,0 +1,26 @@
+import { assert, assertFalse } from "@std/assert";
+import { isPalindromeNumber } from "./panlindrome_number.ts";
+
+Deno.test("Should be return false", () => {
+  const notPalindromeNumber = isPalindromeNumber(10);
+
+  assertFalse(notPalindromeNumber);
+});
+
+Deno.test("Should be return true", () => {
+  const palindromeNumber = isPalindromeNumber(707);
+
+  assert(palindromeNumber);
+});
+
+Deno.test("It is a number less than to 0. Therefore it should return false", () => {
+  const notPalindromeNumber = isPalindromeNumber(-707);
+
+  assertFalse(notPalindromeNumber);
+});
+
+Deno.test("It is a letter. Therefore NaN. It should return false", () => {
+  const notPalindromeNumber = isPalindromeNumber("A707" as unknown as number);
+
+  assertFalse(notPalindromeNumber);
+});
