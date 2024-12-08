820ea4c1437d5e2fe03a6f89d258ca4e9cd6bf44
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-08 17:45:31 -0500

    [C] The `palindrome string` is solved

---
+++ C/palindrome_string.c
@@ -0,0 +1,63 @@
+// =============================================================================
+// Palindrome String (Cadenas palindromos)
+// =============================================================================
+//
+// **Enunciado en Inglés:**
+// Given a string x, return true if x is a palindrome, and false otherwise.
+//
+// **Enunciado en Español:**
+// Dado una cadena de texto x, devuelve verdadero si x es un palíndromo y falso
+// en caso contrario.
+//
+// **Ejemplo:**
+// ```c
+// Input: x = "Oso"
+// Output: true
+// ```
+// =============================================================================
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+const int MAX_CHARACTER = 100;
+const int MINUS_ONE = 1;
+
+bool string_is_palindrome() {
+  char string[MAX_CHARACTER];
+  bool is_palindrome = true;
+
+  printf("Ingresa la palabra a validar: ");
+  fgets(string, MAX_CHARACTER, stdin);
+
+  int length = strlen(string) - 1;
+
+  if (length <= 1) {
+    return true;
+  }
+
+  for (int i = 0; i < length / 2; i++) {
+    char opposite_char = tolower(string[length - i - MINUS_ONE]);
+    char current_char = tolower(string[i]);
+
+    if (current_char != opposite_char) {
+      is_palindrome = false;
+      break;
+    }
+  }
+
+  return is_palindrome;
+}
+
+int main() {
+  bool is_palindrome = string_is_palindrome();
+
+  if (is_palindrome) {
+    printf("La cadena es un palindromo\n");
+  } else {
+    printf("La cadena no es un palindromo\n");
+  }
+
+  return (0);
+}
