bbcb174da4fe9df6b650e5d3e86b95e3d9c02680
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-09 20:29:35 -0500

    [C] Resolve an exercism `Counting the vowels`

---
+++ C/counting_vowels.c
@@ -0,0 +1,31 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+const int MAX_CHARACTER = 100;
+
+void counting_vowels() {
+  char word[MAX_CHARACTER];
+  int amount_vowels = 0;
+
+  printf("Ingresa una palabra: ");
+  fgets(word, MAX_CHARACTER, stdin);
+
+  int length = strlen(word) - 1;
+
+  for (int i = 0; i < length; i++) {
+    word[i] = tolower(word[i]);
+
+    if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' ||
+        word[i] == 'u') {
+      amount_vowels++;
+    }
+  }
+
+  printf("This is the numbers of vowels your word has: %d\n", amount_vowels);
+}
+
+int main() {
+  counting_vowels();
+  return (0);
+}
