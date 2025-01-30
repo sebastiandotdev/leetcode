f10cd5446a9999d420e9f89ca1bb1ad025125669
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-30 10:11:35 -0500

    Learn variables and constants

--- Kotlin/Main.kt
+++ Kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
     println("Hello world")
+
+    val POPCORN = 10
+    val HOTDOG = 7
+    var customers = 10
+
+    println(customers)
+
+    customers = 8
+
+    println(customers)
+    println("There are ${customers + 1} customers")
+
+    println(HOTDOG)
+    println(POPCORN)
+
+    /** Complete the code to make the program print "Mary is 20 years old" to standard output: */
+    val name = "Mary"
+    val age = 20
+
+    println("$name is $age years old")
 }
