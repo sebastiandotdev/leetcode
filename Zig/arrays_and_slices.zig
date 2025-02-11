2b69d3c472c7b9b356f60eecd688254e605377c2
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-10 23:24:15 -0500

    [Zig] Learn Arrays and Slices

---
+++ Zig/arrays_and_slices.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn main() void {
+    const a = [5]i32{ 1, 2, 3, 4, 5 };
+
+    // Con diferente Sintax
+    const b: [5]i32 = .{ 1, 2, 3, 4, 5 };
+    const pointer_value = a[1..4];
+
+    // Inferencia en la longitud durante tiempo de compilación
+    const c = [_]i32{ 1, 2, 3, 4, 5 };
+
+    // Dará un error en tiempo de compilación ya que `pointer_value` al final esta apuntando a la longitud de la variable `a`
+    // Y a es una matriz de numeros contantes para lograr que esta linea funcione debemos cambiar `const` por `var`  en la variable `a`
+    // Pero la constancia de b se relaciona consigo misma, no con los datos a los que apunta.
+    // pointer_value[0] = 99;
+
+    // b = b[1..]; Esto no funciona por que `b` es `const`
+
+    std.debug.print("Types: {} {} {}\n", .{ @TypeOf(a), @TypeOf(b), @TypeOf(c) });
+    std.debug.print("{any}", .{pointer_value});
+}
