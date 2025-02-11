bf1b567f1bda2562505db89c4b955485b958692a
Sebastián García <sebastiandotdev@gmail.com>
2025-02-10 21:50:15 -0500

    Enable Github actions (#1)
    * Enable Github actions
    
    * Add scripts checking

---
+++ Python/test_palindrome_string.py
@@ -0,0 +1,17 @@
+import unittest
+from palindrome_string import is_palindrome_string
+
+
+class TestPalindromeString(unittest.TestCase):
+    def test_should_return_false(self):
+        not_is_palindrome = is_palindrome_string("Alucard")
+
+        self.assertEqual(not_is_palindrome, False)
+
+    def test_should_return_true(self):
+        is_palindrome = is_palindrome_string("Oso")
+
+        self.assertEqual(is_palindrome, True)
+
+if __name__ == "__main__":
+    unittest.main()
