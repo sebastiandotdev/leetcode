d3dd0296320b3ad3fe38a3616da4df086af272ac
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-11 23:24:04 -0500

    [Zig] Preview error handlers

---
+++ Zig/errors.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+const OpenError = error{
+    AccessDenied,
+    NotFound,
+};
+
+pub const Save = struct {
+    lives: u8,
+    level: u16,
+
+    pub fn loadLast() !?Save {
+        return null;
+    }
+
+    pub fn blank() Save {
+        return .{
+            .lives = 3,
+            .level = 1,
+        };
+    }
+};
+
+pub fn main() !void {
+    const save = (try Save.loadLast()) orelse Save.blank();
+    std.debug.print("{any}\n", .{save});
+}
